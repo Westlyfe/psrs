b0fabfe8cdbffd92dff1c2563c96e59e4a7bf4a2
davidbomani1@gmail.com <davidbomani1Gmail.com>
2026-01-13 23:10:21 +0300

    second day discussion

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from hospital import views
+
 
 urlpatterns = [
-    
+    path("",views.index,name="index"),
+    path("create-user/",views.create_user,name="create-user"),
+    path("home/",views.home,name="home"),
+    path("login/",views.login,name="login"),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from hospital.models import User,Doctor,Apointment
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+def index(request):
+    user_visted = "Test User"
+    context = {
+        "user":user_visted
+    }
+    return HttpResponse(render(request,"index.html",context=context))
+
+def home(request):
+    print(request.user)
+    return HttpResponse(render(request,"home.html",context={"user":request.user}))
+
+def create_user(request):
+    if (request.method == 'POST'):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        role = request.POST.get('role')
+        email = request.POST.get('email')
+
+        if(User.objects.filter(username=username).exists()):
+            return HttpResponse({"detail":"Username already exist"})
+        if(User.objects.filter(email = email).exists()):
+            return HttpResponse({"detail":"Email Already exists"})
+        
+        user = User.objects.create(username=username,first_name=first_name,last_name=last_name,email=email,role=role)
+        user.set_password(password)
+        user.save()
+
+        return redirect(to="login")#login page
+    
+    return HttpResponse(render(request,"register.html"))
+
+
+def login(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = authenticate(username = username,password = password)
+            if user is not None:
+                print("Logged succesfully")
+                return redirect(to="home")
+            return redirect(to="login")
+        except:
+            return redirect("login"),{"message":"Invalid credentials"}
+
+
+    return HttpResponse(render(request,"login.html"))
